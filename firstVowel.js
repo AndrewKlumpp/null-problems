aab6bc230d946bd9066137e42d4cf1be45ad5a45
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-12 16:30:24 -0700

    finished firstVowel

--- firstVowel.js
+++ firstVowel.js
@@ -1,9 +1,21 @@
-// your code here
+function firstVowel(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (isVowel(str[i])) {
+      return str[i];
+    }
+  }
+  return null;
+}
 
-
-
-
-
+function isVowel(word) {
+  let vowels = "aeiou";
+  for (i = 0; i < word.length; i++) {
+    char = word[i];
+    if (vowels.includes(char)) {
+      return true;
+    }
+  }
+};
 
 console.log(firstVowel('battery')); // 'a'
 console.log(firstVowel('tunnel')); // 'u'
