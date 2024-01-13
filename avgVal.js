83577f0b83abf172b7cd41832386dc0c80eb60dd
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-13 11:28:10 -0700

    finished avgVal

--- avgVal.js
+++ avgVal.js
@@ -1,10 +1,13 @@
-// your code here
-
-
-
-
-
-
+function avgVal(nums) {
+  if (nums.length === 0) {
+    return null;
+  }
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum = sum + nums[i];
+  }
+  return sum / nums.length;
+}
 
 console.log(avgVal([5, 10])); // 7.5
 console.log(avgVal([3, 7, 2, 1, 2])); // 3
