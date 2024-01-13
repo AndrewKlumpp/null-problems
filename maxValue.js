094795bdaa003df387a5c870b1eb1e23da1c6242
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-13 11:47:31 -0700

    finished maxValue

--- maxValue.js
+++ maxValue.js
@@ -1,8 +1,16 @@
-// your code here
+function maxValue(nums) {
+  if (nums.length === 0) {
+    return null;
+  }
+  let highestNum = nums[0];
+  for (let i = 0; i < nums.length; i++) {
 
-
-
-
+    if (nums[i] > highestNum) {
+      highestNum = nums[i];
+    }
+  }
+  return highestNum;
+}
 
 console.log(maxValue([4, 6, 3, 5, 42, 4])); // 42
 console.log(maxValue([-2, -3, -7, 3 ])); // 3
